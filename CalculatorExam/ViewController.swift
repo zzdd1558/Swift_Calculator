681b4d987c29b1e027bf81d3bfe9f45e821620c2
yunjin choi <zzdd1558@naver.com>
2018-07-05 20:51:00 +0900

    swift를 이용한 계산기 만들기

---
+++ CalculatorExam/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  CalculatorExam
+//
+//  Created by yunjin_choi on 2018. 6. 25..
+//  Copyright © 2018년 yunjin_choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var resultSum: UILabel!
+    var tmpSum : Int = 0
+    var displayText : String = ""
+    var op : String = ""
+    var first :String = ""
+    var second:String = ""
+    let opArray : Array = ["+" , "-" , "*" , "%" , "C" , "="]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("시작")
+    
+    }
+   
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func numberClickEvent( _ sender : UIButton) {
+        let number = sender.titleLabel?.text
+        
+        if opArray.contains(number!) {
+            if number! != "=" {
+                op = number!
+                first = resultSum.text!
+                resultSum.text = ""
+            }else {
+                calculator(first , resultSum.text! )
+            }
+            
+        }else {
+            if resultSum.text == "0"  || resultSum.text == ""{
+                displayText = (sender.titleLabel?.text)!
+            }else {
+                displayText += (sender.titleLabel!.text)!
+            }
+            resultSum.text = displayText
+        }
+       
+    }
+   
+    func calculator(_ value1:String , _ value2 : String ){
+        switch op {
+        case "+":
+            resultSum.text = "\(Int(value1)!+Int(value2)!)"
+        default:
+            print("error")
+        }
+    }
+    
+    
+    
+}
+
